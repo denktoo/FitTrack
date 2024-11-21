378062075fd43c30cd5ac81229b06a3f6876381b
denis-ktoo <146120658+denis-ktoo@users.noreply.github.com>
2024-11-21 16:57:20 +0300

    fix typo

--- obj/Debug/net8.0/FitTrack.AssemblyInfo.cs
+++ obj/Debug/net8.0/FitTrack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6de9ab271b2fbca40503ce2d604664638192f792")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

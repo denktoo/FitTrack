feab3c583d29cbe9102c84c7f173ab970e426c0e
denktoo <146120658+denktoo@users.noreply.github.com>
2024-12-17 15:14:42 +0300

    Update About and Responsiveness in Admin Dashboard

--- obj/Debug/net8.0/FitTrack.AssemblyInfo.cs
+++ obj/Debug/net8.0/FitTrack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66d962154d3e4aef05195dd40970ad461c4edf5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b821d467c33be6e1f4f12c4788376c57679d1678")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

4307c2adce1897451345225ec76202ae3deddf9c
denktoo <146120658+denktoo@users.noreply.github.com>
2024-12-17 22:06:31 +0300

    Add Hamburger Button, and Update Multiple Pages

--- obj/Debug/net8.0/FitTrack.AssemblyInfo.cs
+++ obj/Debug/net8.0/FitTrack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feab3c583d29cbe9102c84c7f173ab970e426c0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfe8334973f80745282152bb7d82d266ec46284d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
